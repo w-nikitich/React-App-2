8d07dcc92411479d750ee6fb194dca78602b5dcd
Nikitich <elderlywitchy@gmail.com>
2024-04-10 10:39:59 +0300

    fix desks component

--- client/src/components/Desks.tsx
+++ client/src/components/Desks.tsx
@@ -13,116 +13,108 @@ import { getAllDesks, updateDesk } from "../requests/desk.requests";
 import Actions from "./Actions";
 import { DeleteDesk, EditDesk } from "../stories/Actions/Actions.stories";
 
-interface IState {
-  actions: string;
-  isActionsOpened: boolean;
-  isEditMode: boolean;
-  name: string;
-}
-
 function Desks() {
   const desks = useSelector((state: RootState) => state.desk.desks);
   const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const updateState = (newState: Partial<IState>): void =>
-    setState((prevState) => ({ ...prevState, ...newState }));
-  const [state, setState] = useState<IState>({
-    actions: "hidden",
-    isActionsOpened: false,
-    isEditMode: false,
-    name: "New Desk",
-  });
 
   useEffect(() => {
-    getAllDesks().then((res) => {
-      dispatch(getDesks(res));
-    });
+    const fetchData = async () => {
+      const allDesks = await getAllDesks();
+      dispatch(getDesks(allDesks));
+    };
 
-    if (state.isActionsOpened) {
-      updateState({ actions: "visible" });
-    } else {
-      updateState({ actions: "hidden" });
-    }
-  }, [dispatch, state.isActionsOpened, state.name]);
-
-  function openDesk(desk: any) {
-    navigate(`/desk/${desk.id}`);
-  }
-
-  function handleActions() {
-    updateState({ isActionsOpened: !state.isActionsOpened });
-  }
-
-  async function handleKeyDown(e: any, id: number) {
-    if (e.key === "Enter") {
-      const newData = await updateDesk(id, { name: state.name });
-      dispatch(updateDeskData({ data: newData }));
-      updateState({ isEditMode: false });
-      console.log(newData);
-    }
-  }
+    fetchData();
+  }, [dispatch]);
 
   return (
     <div className="desks">
       <Button {...DeskCreationSecondary.args} />
 
       {desks.map((desk) => {
-        return (
-          <div className="desk-block">
-            <div
-              className="actions"
-              onClick={() => {
-                handleActions();
-              }}
-            >
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-
-            {state.actions === "visible" ? (
-              <div>
-                <Actions
-                  id={desk.id}
-                  {...EditDesk.args}
-                  onClick={() => {
-                    updateState({ isEditMode: true });
-                  }}
-                />
-                <Actions id={desk.id} {...DeleteDesk.args} />
-              </div>
-            ) : null}
-
-            <div
-              className="desk-element"
-              onClick={() => {
-                openDesk(desk);
-              }}
-            >
-              <div className="desk-data">
-                {state.isEditMode ? (
-                  <input
-                    className="desk-data-change"
-                    type="text"
-                    value={state.name}
-                    onClick={(e) => {
-                      e.stopPropagation();
-                    }}
-                    onChange={(e) => {
-                      updateState({ name: e.target.value });
-                    }}
-                    onKeyDown={async (e) => await handleKeyDown(e, desk.id)}
-                  />
-                ) : (
-                  <h2 className="desk-name">{desk.name}</h2>
-                )}
-                <p className="desk-amount">You have {desk.amount} lists here</p>
-              </div>
-            </div>
-          </div>
-        );
+        return <Desk deskId={desk.id} desk={desk} />;
       })}
+    </div>
+  );
+}
+
+function Desk(desk: any, deskId: number) {
+  const navigate = useNavigate();
+  const [isActionsOpened, setIsActionsOpened] = useState(false);
+  const [actions, setActions] = useState("hidden");
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [name, setName] = useState(desk.desk.name);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isActionsOpened) {
+      setActions("visible");
+    } else {
+      setActions("hidden");
+    }
+  }, [isActionsOpened]);
+
+  function openDesk() {
+    navigate(`/desk/${desk.deskId}`);
+  }
+
+  function handleActions() {
+    setIsActionsOpened(!isActionsOpened);
+  }
+
+  async function handleKeyDown(e: any, id: number) {
+    if (e.key === "Enter") {
+      const newData = await updateDesk(id, { name: name });
+      dispatch(updateDeskData({ data: newData }));
+      setIsEditMode(false);
+    }
+  }
+
+  return (
+    <div className="desk-block">
+      <div
+        className="actions"
+        onClick={() => {
+          handleActions();
+        }}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <div className={`${actions}`}>
+        <Actions
+          id={desk.deskId}
+          {...EditDesk.args}
+          onClick={() => {
+            setIsEditMode(true);
+          }}
+        />
+        <Actions id={desk.deskId} {...DeleteDesk.args} />
+      </div>
+
+      <div className="desk-element" onClick={openDesk}>
+        <div className="desk-data">
+          {isEditMode ? (
+            <input
+              className="desk-data-change"
+              type="text"
+              value={name}
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              onKeyDown={async (e) => await handleKeyDown(e, desk.deskId)}
+            />
+          ) : (
+            <h2 className="desk-name">{desk.desk.name}</h2>
+          )}
+          <p className="desk-amount">You have {desk.desk.amount} lists here</p>
+        </div>
+      </div>
     </div>
   );
 }
